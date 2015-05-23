02ef911aa7b4238bbf40bfff754a31700d79bfc7
KururuLABO <nxeapher@hotmail.com>
2015-05-24 05:19:56 +0700

    Add class PlaceInfo for wrapper information from google json.

---
+++ app/src/main/java/com/ossyria/placefinder/PlaceInfo.java
@@ -0,0 +1,55 @@
+package com.ossyria.placefinder;
+
+/**
+ * Created by KururuLABO on 5/24/2015.
+ */
+public class PlaceInfo {
+    private String id;
+    private String icon;
+    private String name;
+    private String vicinity;
+    private Double latitude;
+    private Double longitude;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getIcon() {
+        return icon;
+    }
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+    public Double getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+    public Double getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getVicinity() {
+        return vicinity;
+    }
+    public void setVicinity(String vicinity) {
+        this.vicinity = vicinity;
+    }
+
+    @Override
+    public String toString() {
+        return "Place{" + "id=" + id + ", icon=" + icon + ", name=" + name + ", latitude=" + latitude + ", longitude=" + longitude + '}';
+    }
+}
